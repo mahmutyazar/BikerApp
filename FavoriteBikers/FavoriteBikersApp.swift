f82fa3d2f29535546c4a52ceded66480f2599c1f
Mahmut Yazar <yazarmahmut34@gmail.com>
2022-07-07 15:14:48 +0300

    Biker App
    A very simple app to introduce famous bikers.

---
+++ FavoriteBikers/FavoriteBikersApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavoriteBikersApp.swift
+//  FavoriteBikers
+//
+//  Created by Mahmut Yazar on 7.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct FavoriteBikersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
